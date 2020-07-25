a7b12e271d749fab307956279c75ff60897f174d
It's Me <8347078+mrg0lden@users.noreply.github.com>
2020-07-25 14:17:00 +0300

    build: upgrade rollup plugins

--- rollup.config.js
+++ rollup.config.js
@@ -1,7 +1,7 @@
-import commonjs from "rollup-plugin-commonjs";
+import commonjs from "@rollup/plugin-commonjs";
 import VuePlugin from "rollup-plugin-vue";
-import resolve from "rollup-plugin-node-resolve";
-import babel from "rollup-plugin-babel";
+import resolve from "@rollup/plugin-node-resolve";
+import babel from "@rollup/plugin-babel";
 
 export default {
   input: "src/index.js",
